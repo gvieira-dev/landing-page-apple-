5d183779b9c6c4c1b3cb029ea13b9f0f3cfe4e49
Gustavo Vieira <gustavovieira0806fnafsls@gmail.com>
2025-12-09 12:10:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlide(e){
+    document.querySelector('#iPhone').src = e;
+}
+
+function cicleChange(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
